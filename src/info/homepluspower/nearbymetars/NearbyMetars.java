436f04809bbaed563d963150bdfd8592effc328e
Keith Mendoza <pantherse@gmail.com>
2011-03-14 16:02:09 -0700

    Now have a "Get Metar" menu item. Created icons

--- src/info/homepluspower/nearbymetars/NearbyMetars.java
+++ src/info/homepluspower/nearbymetars/NearbyMetars.java
@@ -9,7 +9,11 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
@@ -74,10 +78,6 @@ public class NearbyMetars extends MapActivity implements LocationListener {
         mapView = (MapView)findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         mapView.preLoad();
-        
-        Log.d("NearbyMetars", "Retrieve data from last known location");
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        getMetarData(location);
     }
     
     @Override
@@ -122,5 +122,30 @@ public class NearbyMetars extends MapActivity implements LocationListener {
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+	
+	//Menu stuff
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.getmetar:
+			GeoPoint center = mapView.getMapCenter();
+			Location location = new Location("");
+			location.setLatitude(center.getLatitudeE6()/1e6);
+			location.setLongitude(center.getLongitudeE6()/1e6);
+			getMetarData(location);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
