72463c7156f66738e1be87e73d1702b5624767a1
Keith Mendoza <pantherse@gmail.com>
2011-03-14 11:22:51 -0700

    Fixed how syncing of metar retrieval is handled

--- src/info/homepluspower/nearbymetars/NearbyMetars.java
+++ src/info/homepluspower/nearbymetars/NearbyMetars.java
@@ -1,6 +1,8 @@
 package info.homepluspower.nearbymetars;
 
 import java.util.List;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 
 import android.location.Location;
 import android.location.LocationListener;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
@@ -20,6 +23,47 @@ public class NearbyMetars extends MapActivity implements LocationListener {
 	
 	private MetarDataRetriever dataRetrieverTask;
 	
+	/**
+     * Do the retrieval and parsing of metar data. This assumes that metarList will be instantiated by someone else
+     * @param location
+     */
+    private void parseMetarData(Location location) {
+    	if(dataRetrieverTask != null) {
+    		Log.d("NearbyMetars", "Cancelling existing metar processing");
+    		if(dataRetrieverTask.getStatus() == MetarDataRetriever.Status.RUNNING)
+    		{
+    			dataRetrieverTask.cancel(true);
+    			try {
+    				Log.d("NearbyMetars", "Waiting for existing retrieval to return");
+					dataRetrieverTask.get();
+				} catch (InterruptedException e) {
+				} catch (ExecutionException e) {
+				} catch (CancellationException e) {					
+				}
+				Log.d("NearbyMetars", "Existing retrieval back");
+    		}
+    		else
+    			Log.d("NearbyMetars", "Existing retrieval already cancelled");
+    	}
+    	
+    	Log.d("NearbyMetars", "Start retrieving new data");
+    	dataRetrieverTask = (MetarDataRetriever)new MetarDataRetriever().execute(metarList, location);
+    }
+    
+    private void getMetarData(Location location) {
+    	if(metarList == null) {
+    		Log.d("NearbyMetars", "First time loading MetarList");
+    		metarList = new MetarList(getResources().getDrawable(R.drawable.overlaydefault), mapView.getContext());
+    		parseMetarData(location);
+    		List<Overlay> mapOverlays = mapView.getOverlays();
+            mapOverlays.add(metarList);
+    	}
+    	else {
+    		Log.d("NearbyMetars", "Refreshing existing MetarList");
+    		parseMetarData(location);
+    	}
+    }
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +73,11 @@ public class NearbyMetars extends MapActivity implements LocationListener {
         
         mapView = (MapView)findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
-        mapView.setReticleDrawMode(MapView.ReticleDrawMode.DRAW_RETICLE_OVER);
         mapView.preLoad();
+        
+        Log.d("NearbyMetars", "Retrieve data from last known location");
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        getMetarData(location);
     }
     
     @Override
@@ -42,45 +89,30 @@ public class NearbyMetars extends MapActivity implements LocationListener {
     protected void onResume() {
     	super.onResume();
     	Log.d("NearbyMetars", "Resume");
-    	dataRetrieverTask = null;
     	
     	locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 300000L, 1609.344f, this);
     	locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 300000L, 1609.344f, this);
-    	
-//    	metarList = new MetarList(getResources().getDrawable(R.drawable.overlaydefault), mapView.getContext());
-//		new MetarDataRetriever().execute(metarList, locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
     }
     
-    private void cancelMetarProcessing() {
-    	if(dataRetrieverTask != null) {
-			Log.d("NearbyMetars", "Cancelling existing metar processing");
-			dataRetrieverTask.cancel(true);
-			dataRetrieverTask = null;
-		}	
-    }
     @Override
     protected void onPause() {
     	super.onPause();
     	Log.d("NearbyMetars", "Pause");
-    	cancelMetarProcessing();
+    	
+    	if(dataRetrieverTask != null) {
+    		dataRetrieverTask.cancel(true);
+    		dataRetrieverTask = null;
+    	}
+    	
     	locationManager.removeUpdates(this);
     }
 
     public void onLocationChanged(Location location) {
 		Log.d("NearbyMetars", "Got new location");
-		mapView.getController().animateTo(MetarItem.coordsToGeoPoint(location.getLatitude(), location.getLongitude()));
-		
-		if(metarList == null) {
-			List<Overlay> mapOverlays = mapView.getOverlays();
-	        metarList = new MetarList(getResources().getDrawable(R.drawable.overlaydefault), mapView.getContext());
-	        mapOverlays.add(metarList);	
-		}
-		
-		cancelMetarProcessing();
-		dataRetrieverTask = new MetarDataRetriever();
-		dataRetrieverTask.execute(metarList, location);
-		
-		Log.d("NearbyMetars", "Done handling onLocationChanged");
+		MapController controller = mapView.getController();
+		controller.animateTo(MetarItem.coordsToGeoPoint(location.getLatitude(), location.getLongitude()));
+		controller.setZoom(10);
+		getMetarData(location);
 	}
 
 	public void onProviderDisabled(String provider) {
